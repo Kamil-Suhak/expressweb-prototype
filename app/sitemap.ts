8cec05712ee5d90c8391b99aa26f03f37a7f800f
Kamil <kamilsuhak@gmail.com>
2026-01-11 02:09:18 +0100

    seo optimization

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://expressweb-prototype.vercel.app",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ];
+
+  //   example dynamic:
+  // const routes = ['', '/about', '/contact', '/services'].map((route) => ({
+  //     url: `${siteConfig.url}${route}`,
+  //     lastModified: new Date().toISOString(),
+  //     changeFrequency: 'monthly' as const,
+  //     priority: route === '' ? 1 : 0.8,
+  //   }));
+}
